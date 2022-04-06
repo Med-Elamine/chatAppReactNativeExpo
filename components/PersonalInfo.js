3f61cd8f677ac4a3488daa9d30419b8de4c89935
ELKHAYYAT MOHAMMAD ELAMINE <96497970+Med-Elamine@users.noreply.github.com>
2022-04-06 14:08:47 +0000

    Add pick an Image component

--- components/PersonalInfo.js
+++ components/PersonalInfo.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import { View, TextInput, Text, Image, Button  } from "react-native";
+import ImageChooser from "./ImageChooser";
 import Styles from "./Styles";
 
 const PersonalInfo = () => {
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("");
+
     return(
         <View style={Styles.personalInfoContainer}>
 
@@ -10,8 +14,10 @@ const PersonalInfo = () => {
 
             <View style={Styles.enterYourName}>
                 <Text style={Styles.nameText}>Enter the User Name</Text>
-                <TextInput style={Styles.nameTextInput}></TextInput>
+                <TextInput style={Styles.nameTextInput} onChangeText={(text) => setName(text)} value={name}></TextInput>
             </View>
+
+            <ImageChooser onChangeImage={(image) => setImage(image)} /> 
 
             <Button title="Start Chating" onPress={() => {} }/>
 
