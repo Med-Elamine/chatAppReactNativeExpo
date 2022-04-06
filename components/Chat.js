79ba6998135889b6739c8f292e6b50d5c8f0e142
ELKHAYYAT MOHAMMAD ELAMINE <96497970+Med-Elamine@users.noreply.github.com>
2022-04-06 16:35:48 +0000

    Update still there a error to handle with in chat.js & chatItem.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
-import {  View, Text, Button } from "react-native";
+import {  View, Button, FlatList, TextInput } from "react-native";
+import Styles from "./Styles";
 
 const Chat = (props) => {
+    const [chatInput, setChatInput] = useState("");
+
     return(
-        <View>
-            <Text>Hi {props.username} You can chat here shortly</Text>
+        <View style={Styles.container}>
+
+        <FlatList></FlatList>
+
+        <View style={Styles.sendSection}>
+            <TextInput
+            style={Styles.chatTextInput}
+            value={chatInput}
+            onChangeText={(text) => setChatInput(text)}
+            />
+            <Button
+            title="Send"
+            onPress={() => {
+                setChatInput("");
+            }}
+            ></Button>
+        </View>
         </View>
     );
 }
